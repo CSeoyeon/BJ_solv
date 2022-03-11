a5db892d593dc0c223810545a960461cf2a49c57
Seoyeon Choi <65939546+CSeoyeon@users.noreply.github.com>
2022-03-11 16:22:15 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 216 ms, Memory: 17740 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int year = scanner.nextInt();
+        if(year>=1000 && 3000>=year)
+            {System.out.println(year-543);
+            }
+    }
+}
