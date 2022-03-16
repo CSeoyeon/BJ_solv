e1fa0084d49d7651580870b262cd8a915b23def4
Seoyeon Choi <65939546+CSeoyeon@users.noreply.github.com>
2022-03-16 23:55:09 +0900

    [Bronze I] Title: 평균은 넘겠지, Time: 444 ms, Memory: 32868 KB -BaekjoonHub

---
+++ 백준/Bronze/4344. 평균은 넘겠지/평균은 넘겠지.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int c = scanner.nextInt();
+
+        for(int i=0; i<c; i++){
+            int student = scanner.nextInt();
+            int[] score = new int[student];
+
+            int sum = 0;
+            double mean = 0;
+            int count =0; 
+
+            for(int j=0; j<student; j++){
+                score[j] = scanner.nextInt();
+                sum += score[j];
+            }
+
+            mean = sum / student;
+            for(int k =0; k<student; k++){
+                if(score[k] > mean){
+                    count ++; 
+                }
+            }
+            System.out.printf("%.3f", (double)count/student*100);
+            System.out.println("%");
+
+        }
+
+        scanner.close();
+        
+    
+        
+    }
+}
