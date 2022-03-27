a275a7d092cfb7679b939f92b4249866fe3eb8ae
Seoyeon Choi <65939546+CSeoyeon@users.noreply.github.com>
2022-03-27 14:45:54 +0900

    [Silver IV] Title: 소수 찾기, Time: 216 ms, Memory: 17820 KB -BaekjoonHub

---
+++ 백준/Silver/1978. 소수 찾기/소수 찾기.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int count =0;
+
+        for(int i =0; i<n; i++){
+            int num = scanner.nextInt();
+            boolean check = true;
+            // 0, 1 은 소수가 아니다.  
+            if (num == 1 || num == 0){
+               check = false; 
+            } 
+
+            for(int j =2; j<= Math.sqrt(num); j++){  
+                if(num % j == 0){
+                    check = false;
+                }
+            }    
+            if(check == true){count++;}
+        }
+        scanner.close();
+        System.out.println(count);
+    }
+}
+        
+      
