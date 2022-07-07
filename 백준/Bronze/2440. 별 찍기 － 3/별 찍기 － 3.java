34db24d07f9e2ed13ad81ad7a083ac8ae8ce54a1
Seoyeon Choi <65939546+CSeoyeon@users.noreply.github.com>
2022-07-07 23:29:04 +0900

    [Bronze IV] Title: 별 찍기 - 3, Time: 132 ms, Memory: 14192 KB -BaekjoonHub

---
+++ 백준/Bronze/2440. 별 찍기 － 3/별 찍기 － 3.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException{
+    
+
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int i = Integer.parseInt(br.readLine());
+    while(true){
+        System.out.println("*".repeat(i));
+        i--;
+        if(i == 0){break;}
+    }
+
+    }
+}
+    
