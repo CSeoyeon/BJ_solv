0e34e880fca58ce1ad0b475256d716bad3cd9674
Seoyeon Choi <65939546+CSeoyeon@users.noreply.github.com>
2022-07-09 15:08:34 +0900

    [Bronze IV] Title: 전자레인지, Time: 176 ms, Memory: 16328 KB -BaekjoonHub

---
+++ 백준/Bronze/10162. 전자레인지/전자레인지.java
@@ -0,0 +1,51 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.lang.reflect.Array;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException{
+
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    
+    int t = Integer.parseInt(br.readLine());
+
+    int a = 0;
+    int b = 0; 
+    int c = 0;
+
+
+    while(true){
+        if(t>= 300){
+            t -= 300;
+            a++;
+        }
+        else if(t>= 60){
+           t-=60;
+           b++;
+        }
+        else if(t>= 10){
+            t-=10;
+            c++;
+        }
+
+        if (t==0){
+            System.out.println(a + " " + b + " " + c);
+            break; 
+            
+        }
+        if(t%10 != 0 ){
+            System.out.println(-1);
+            break;
+        }
+    
+    }
+
+    
+
+
+    }
+}
+    
