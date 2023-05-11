9ffc871ea70a2d746f44231f1f72ecd05df15d0a
Seoyeon Choi <65939546+CSeoyeon@users.noreply.github.com>
2023-05-11 23:46:11 +0900

    [level 0] Title: 편지, Time: 0.13 ms, Memory: 76.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120898. 편지/편지.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String message) {
+        int answer = 0;
+        String[] m = message.split("");
+        answer = m.length *2;
+        return answer;
+        
+    }
+}
