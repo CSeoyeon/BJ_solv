cf5510aed84e3147b8d76aaec391ef29385d3cfb
Seoyeon Choi <65939546+CSeoyeon@users.noreply.github.com>
2025-01-14 12:38:09 +0900

    [Silver IV] Title: 큐 2, Time: 1244 ms, Memory: 347948 KB -BaekjoonHub

--- 백준/Silver/18258. 큐 2/큐 2.java
+++ 백준/Silver/18258. 큐 2/큐 2.java
@@ -1,102 +1,61 @@
+import javax.swing.plaf.IconUIResource;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
-
+import java.util.*;
 
 public class Main {
-    
-    static int top = 0;
-    static int[] queue;
-    static int tail = -1; 
 
-    public static int push(int data){
-        queue[++tail] = data;
-        return data;
-    }
+    public static void main(String[] args) throws IOException {
 
-    public static int pop(){
-        if (size() == 0){
-            return -1;
-        }
-        else{ 
-            return queue[top++]; }
-    }
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        //Scanner scanner = new Scanner(System.in);
+        int n = Integer.parseInt(br.readLine());
+        Deque<Integer> queue = new LinkedList<>();
+        StringBuilder sb = new StringBuilder();
+        int back =0;
+        StringTokenizer st;
+        while(n--> 0) {
+            st = new StringTokenizer(br.readLine(), " ");
 
-    public static int size(){
-        return tail - top +1;
-    }
-
-    public static int empty(){
-        if (size() == 0){
-            return 1;
-        }
-        else {return 0;}
-    }
-
-    public static int front(){
-        if (size() == 0){
-            return -1;
-        }
-        else{
-            return queue[top];
-        }
-    }
-
-    public static int back(){
-        if (size() == 0){
-            return -1;
-        }
-        else{
-            return queue[tail];
-        }
-    }
-
-
-    public static void main(String[] args) throws IOException{
-
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    StringBuilder sb = new StringBuilder();
-    int n = Integer.parseInt(br.readLine());
-    queue = new int[n];
-    
-    for(int i=0; i<n; i++){
-
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-
-        switch(st.nextToken()){
-            case "push":
-                push(Integer.parseInt(st.nextToken()));
-                break;
-
-            case "pop":
-                sb.append(pop()).append("\n"); 
-                break;
-
-            case "size":    
-                sb.append(size()).append("\n"); 
-                break;
-
-            case "empty":
-                sb.append(empty()).append("\n");
-                break;
-
-            case "front":
-                sb.append(front()).append("\n"); 
-                break;
-
-            case "back":
-                sb.append(back()).append("\n"); 
-                break;
-
+            switch (st.nextToken()) {
+                case "push":
+                    queue.offer(Integer.valueOf(st.nextToken()));
+                    break;
+                case "pop":
+                    if (!queue.isEmpty()) {
+                        sb.append(queue.poll() + "\n");
+                    } else sb.append(-1 +"\n");
+                    break;
+                case "size":
+                    sb.append(queue.size()+ "\n");
+                    break;
+                case "empty":
+                    if (!queue.isEmpty()) {
+                        sb.append(0+"\n");
+                    } else sb.append(1 +"\n");
+                    break;
+                case "front":
+                    if (!queue.isEmpty()) {
+                        sb.append(queue.peek()+"\n");
+                    } else sb.append(-1 +"\n");
+                    break;
+                case "back":
+                    if (!queue.isEmpty()) {
+                        sb.append(queue.peekLast()+"\n");
+                    } else sb.append(-1 +"\n");
+                    break;
             }
-            //System.out.println(sb);
-            //sb.setLength(0);
-           
-        }
 
-      System.out.println(sb);
-    
+        }
+        System.out.println(sb);
     }
+
+
 }
-    
+
+
+
+
+
+
