eabfb4fc2a5e34781de237d3f7c21d8db196be01
Seoyeon Choi <65939546+CSeoyeon@users.noreply.github.com>
2025-03-04 15:05:16 +0900

    [Bronze IV] Title: 베라의 패션, Time: 172 ms, Memory: 17640 KB -BaekjoonHub

---
+++ 백준/Bronze/15439. 베라의 패션/베라의 패션.java
@@ -0,0 +1,19 @@
+import javax.swing.plaf.IconUIResource;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        System.out.println(n*(n-1));
+
+
+    }
+}
+
+
+
